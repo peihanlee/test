e232c9a0dc46f684b301857ea06fc8b0c8fb0787
PEI HAN LEE <eric.jds@gmail.com>
2020-03-31 22:26:40 +0800

    update 173.binary-search-tree-iterator.cpp

---
+++ 173.binary-search-tree-iterator.cpp
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=173 lang=cpp
+ *
+ * [173] Binary Search Tree Iterator
+ *
+ * https://leetcode.com/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (53.33%)
+ * Likes:    2076
+ * Dislikes: 263
+ * Total Accepted:    271.6K
+ * Total Submissions: 506.4K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n[[[7,3,15,null,null,9,20]],[null],[null],[null],[null],[null],[null],[null],[null],[null]]'
+ *
+ * Implement an iterator over a binary search tree (BST). Your iterator will be
+ * initialized with the root node of a BST.
+ * 
+ * Calling next() will return the next smallest number in the BST.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * 
+ * 
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // return 3
+ * iterator.next();    // return 7
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 9
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 15
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 20
+ * iterator.hasNext(); // return false
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * next() and hasNext() should run in average O(1) time and uses O(h) memory,
+ * where h is the height of the tree.
+ * You may assume that next() call will always be valid, that is, there will be
+ * at least a next smallest number in the BST when next() is called.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+      searchLeft(root);
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+      TreeNode *n = st.top();
+      st.pop();
+      if(n!=NULL){
+        searchLeft(n->right);
+        return n->val;
+      } 
+      return 0;      
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+      return !st.empty();
+    }
+private:
+    void searchLeft(TreeNode *r)
+    {
+      while(r!=NULL)
+      {
+        st.push(r);
+        r=r->left;
+      }
+    }
+    stack<TreeNode *> st;    
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+// @lc code=end
