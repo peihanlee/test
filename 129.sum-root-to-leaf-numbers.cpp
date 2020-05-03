c840955931b8d687aaa99a62b3cc95a5cc65ae49
PEI HAN LEE <eric.jds@gmail.com>
2020-05-04 04:23:16 +0800

    update 129.sum-root-to-leaf-numbers.cpp

---
+++ 129.sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=129 lang=cpp
+ *
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (45.77%)
+ * Likes:    1061
+ * Dislikes: 31
+ * Total Accepted:    231.5K
+ * Total Submissions: 503.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * 
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ * 
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int calPathSum(TreeNode *root, string &curNum, int &sum){
+      if(!root){
+        return -1;
+      }
+      int leftNum = 0;
+      int rightNum = 0;
+
+      curNum.push_back(root->val+'0');
+      leftNum=calPathSum(root->left, curNum, sum);
+      rightNum=calPathSum(root->right, curNum, sum);
+      if(leftNum==-1 && rightNum==-1){
+        sum+=stoi(curNum);
+      }
+      curNum.pop_back();
+      return root->val; 
+    }
+
+    int sumNumbers(TreeNode* root) {
+      int sum=0;
+      string curNum="";
+      calPathSum(root, curNum, sum);        
+      return sum;
+    }
+};
+// @lc code=end
