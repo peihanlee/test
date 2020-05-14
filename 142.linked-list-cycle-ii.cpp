9b04f4675dc7b39aa5e17ce2939e7c0dc6d6263d
PEI HAN LEE <eric.jds@gmail.com>
2020-05-15 03:45:49 +0800

    update 142.linked-list-cycle-ii.cpp

---
+++ 142.linked-list-cycle-ii.cpp
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=142 lang=cpp
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (35.60%)
+ * Likes:    2265
+ * Dislikes: 189
+ * Total Accepted:    290.8K
+ * Total Submissions: 811.9K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * Note: Do not modify the linked list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow-up:
+ * Can you solve it without using extra space?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+      ListNode *slow, *fast;
+
+      if(!head || !head->next){
+        return NULL;
+      }
+      fast=head;
+      slow=head;
+
+      while(fast && fast->next){
+        cout<<"slow:"<<slow->val<<" fast:"<<fast->val<<"\n";
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){
+          cout<<"match A:"<<fast->val<<"\n";
+          break;
+        }
+      }
+
+      if(fast==slow){
+        fast=head;
+        while(fast!=slow){
+          fast=fast->next;
+          slow=slow->next;
+        }
+        cout<<"match B:"<<fast->val<<"\n";
+        return fast;
+      }
+
+      return NULL;
+    }
+};
+// @lc code=end
