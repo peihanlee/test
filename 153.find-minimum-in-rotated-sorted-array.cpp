ec629a49d0fff15cddde06cdc8d789b95776e9bb
PEI HAN LEE <eric.jds@gmail.com>
2020-05-19 02:26:32 +0800

    update 153.find-minimum-in-rotated-sorted-array.cpp

--- 153.find-minimum-in-rotated-sorted-array.cpp
+++ 153.find-minimum-in-rotated-sorted-array.cpp
@@ -42,7 +42,7 @@
 class Solution {
 public:
     int getMin(vector<int>& nums, int l, int r){
-      if(l==r) return nums[l];
+      if(nums[l]<=nums[r]) return nums[l];
       int mid=(l+r)/2;
 
       return min(getMin(nums,l,mid),getMin(nums,mid+1,r));
