5b9a116cb8fc8f783b276f47eae7168872eb049b
PEI HAN LEE <eric.jds@gmail.com>
2020-05-20 01:43:43 +0800

    update 168.excel-sheet-column-title.cpp

---
+++ 168.excel-sheet-column-title.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=168 lang=cpp
+ *
+ * [168] Excel Sheet Column Title
+ *
+ * https://leetcode.com/problems/excel-sheet-column-title/description/
+ *
+ * algorithms
+ * Easy (30.30%)
+ * Likes:    1027
+ * Dislikes: 219
+ * Total Accepted:    206K
+ * Total Submissions: 677.8K
+ * Testcase Example:  '1'
+ *
+ * Given a positive integer, return its corresponding column title as appear in
+ * an Excel sheet.
+ * 
+ * For example:
+ * 
+ * 
+ * ⁠   1 -> A
+ * ⁠   2 -> B
+ * ⁠   3 -> C
+ * ⁠   ...
+ * ⁠   26 -> Z
+ * ⁠   27 -> AA
+ * ⁠   28 -> AB 
+ * ⁠   ...
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1
+ * Output: "A"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 28
+ * Output: "AB"
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 701
+ * Output: "ZY"
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string convertToTitle(int n) {
+      string res="";
+
+      while(n){
+        res = char('A'+(n-1)%26)+res;
+        n=(n-1)/26;
+      }
+
+      return res;      
+
+    }
+};
+// @lc code=end
