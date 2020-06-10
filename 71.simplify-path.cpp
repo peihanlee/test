b731fb2cabd4d957976f302e3a1040cac756036d
PEI HAN LEE <eric.jds@gmail.com>
2020-06-10 19:52:31 +0800

    update 71.simplify-path.cpp

---
+++ 71.simplify-path.cpp
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=71 lang=cpp
+ *
+ * [71] Simplify Path
+ *
+ * https://leetcode.com/problems/simplify-path/description/
+ *
+ * algorithms
+ * Medium (31.31%)
+ * Likes:    684
+ * Dislikes: 1624
+ * Total Accepted:    194.5K
+ * Total Submissions: 617.5K
+ * Testcase Example:  '"/home/"'
+ *
+ * Given an absolute path for a file (Unix-style), simplify it. Or in other
+ * words, convert it to the canonical path.
+ * 
+ * In a UNIX-style file system, a period . refers to the current directory.
+ * Furthermore, a double period .. moves the directory up a level.
+ * 
+ * Note that the returned canonical path must always begin with a slash /, and
+ * there must be only a single slash / between two directory names. The last
+ * directory name (if it exists) must not end with a trailing /. Also, the
+ * canonical path must be the shortest string representing the absolute
+ * path.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "/home/"
+ * Output: "/home"
+ * Explanation: Note that there is no trailing slash after the last directory
+ * name.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "/../"
+ * Output: "/"
+ * Explanation: Going one level up from the root directory is a no-op, as the
+ * root level is the highest level you can go.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "/home//foo/"
+ * Output: "/home/foo"
+ * Explanation: In the canonical path, multiple consecutive slashes are
+ * replaced by a single one.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "/a/./b/../../c/"
+ * Output: "/c"
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "/a/../../b/../c//.//"
+ * Output: "/c"
+ * 
+ * 
+ * Example 6:
+ * 
+ * 
+ * Input: "/a//b////c/d//././/.."
+ * Output: "/a/b/c"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string simplifyPath(string path) {
+      string res,s;
+      stack<string> st;
+      stringstream ss(path);
+      while(getline(ss,s,'/')){
+        if(s=="" || s==".") continue;
+        else if(s==".." && !st.empty()) st.pop();
+        else if(s!="..") st.push(s);
+        cout<<s<<"\n";
+      }
+
+      while(!st.empty()){
+        res = "/"+st.top()+res;
+        st.pop();
+      }
+
+      return res.empty()?"/":res;
+    }
+};
+// @lc code=end
