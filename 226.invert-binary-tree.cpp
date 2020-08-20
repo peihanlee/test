0eef8af2605465759272521bdd93ba284cfb47c4
PEI HAN LEE <eric.jds@gmail.com>
2020-08-20 14:53:27 +0800

    update 226.invert-binary-tree.cpp

---
+++ 226.invert-binary-tree.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=226 lang=cpp
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (62.05%)
+ * Likes:    2724
+ * Dislikes: 43
+ * Total Accepted:    437.6K
+ * Total Submissions: 702.5K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Invert a binary tree.
+ * 
+ * Example:
+ * 
+ * Input:
+ * 
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ * 
+ * Output:
+ * 
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ * 
+ * Trivia:
+ * This problem was inspired by this original tweet by Max Howell:
+ * 
+ * Google: 90% of our engineers use the software you wrote (Homebrew), but you
+ * can’t invert a binary tree on a whiteboard so f*** off.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+      if(!root) return root;
+
+      TreeNode *tmp;
+
+      tmp=invertTree(root->left);
+      root->left=invertTree(root->right);
+      root->right=tmp;
+
+      return root;
+    }
+};
+// @lc code=end
