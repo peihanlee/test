05cb55571a3cbfe4e9fa46cc70dbd45819d984c7
PEI HAN LEE <eric.jds@gmail.com>
2020-08-27 15:41:34 +0800

    update 229.majority-element-ii.cpp

--- 229.majority-element-ii.cpp
+++ 229.majority-element-ii.cpp
@@ -39,15 +39,34 @@ public:
       vector<int> res;
       int n = nums.size();
       int boundary = n/3;
-      unordered_map<int,int> mp;
+      int a=0;
+      int b=0;
+      int cnt_a=0;
+      int cnt_b=0;
 
       for(int i=0;i<n;i++){
-        mp[nums[i]]++;
-        if(mp[nums[i]]>boundary && mp[nums[i]]<n+1){
-          res.push_back(nums[i]);
-          mp[nums[i]]=n+1;
+        if(a==nums[i]){
+          cnt_a++;
+        }else if(b==nums[i]){
+          cnt_b++;
+        }else if(cnt_a==0) {
+          a=nums[i]; cnt_a++;
+        }else if(cnt_b==0) {
+          b=nums[i]; cnt_b++;
+        }else{cnt_a--; cnt_b--;}
+      }
+
+      cnt_a=0;
+      cnt_b=0;
+      for(int i=0;i<n;i++){
+        if(a==nums[i]){
+          cnt_a++;
+        }else if(b==nums[i]){
+          cnt_b++;
         }
       }
+      if(cnt_a>boundary) res.push_back(a);
+      if(cnt_b>boundary) res.push_back(b);
 
       return res;
     }
