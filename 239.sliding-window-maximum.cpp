632c6a4758251d157e65248896400d7d804a3ebf
PEI HAN LEE <eric.jds@gmail.com>
2020-09-23 16:47:55 +0800

    update 239.sliding-window-maximum.cpp

--- 239.sliding-window-maximum.cpp
+++ 239.sliding-window-maximum.cpp
@@ -53,7 +53,8 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-      list<int> l;
+      //list<int> l;
+      deque<int> l;
       vector<int> res;
 
       for(int i=0;i<nums.size();i++){
