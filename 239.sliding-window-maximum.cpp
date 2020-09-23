4af0647fd13784c9e4a40684240de3d1fce53dd2
PEI HAN LEE <eric.jds@gmail.com>
2020-09-23 16:47:17 +0800

    update 239.sliding-window-maximum.cpp

---
+++ 239.sliding-window-maximum.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=239 lang=cpp
+ *
+ * [239] Sliding Window Maximum
+ *
+ * https://leetcode.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (41.05%)
+ * Likes:    2725
+ * Dislikes: 159
+ * Total Accepted:    233.1K
+ * Total Submissions: 567.3K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * Given an array nums, there is a sliding window of size k which is moving
+ * from the very left of the array to the very right. You can only see the k
+ * numbers in the window. Each time the sliding window moves right by one
+ * position. Return the max sliding window.
+ * 
+ * Follow up:
+ * Could you solve it in linear time?
+ * 
+ * Example:
+ * 
+ * 
+ * Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+ * Output: [3,3,5,5,6,7] 
+ * Explanation: 
+ * 
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+      list<int> l;
+      vector<int> res;
+
+      for(int i=0;i<nums.size();i++){
+        if(!l.empty() && l.front()==i-k) l.pop_front();
+        while(!l.empty() && nums[l.back()]<nums[i]) l.pop_back();
+        l.push_back(i);
+        if(i>=k-1) res.push_back(nums[l.front()]);
+      }
+
+      return res;
+        
+    }
+};
+// @lc code=end
